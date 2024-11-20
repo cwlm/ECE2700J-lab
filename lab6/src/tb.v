b3ed6feb9ffccf33350c30c9398bb1a3089e5dbd
cwlm_ma <cwlm_ma@sjtu.edu.cn>
2024-11-20 15:27:31 +0800

    refactor testbench for lab 6: streamline signal declarations and improve readability

--- lab6/src/tb.v
+++ lab6/src/tb.v
@@ -2,53 +2,54 @@
 
 module tb_main;
 
-    reg clk, reset;
-    reg [3:0] row;
-    wire [3:0] col;
-    wire [6:0] C;
+reg clk, reset;
+wire [3:0] row;
+wire [3:0] col;
+wire [6:0] C;
 
-    main main(
-        .clk(clk),
-        .reset(reset),
-        .row(row),
-        .col(col),
-        .C(C)
-    );
+main main(
+         .clk(clk),
+         .reset(reset),
+         .row(row),
+         .col(col),
+         .C(C)
+     );
 
-    initial begin
-        clk = 0;
-        forever #5 clk = ~clk;
+initial begin
+    clk = 0;
+    forever
+        #5 clk = ~clk;
+end
+
+reg [3:0] i;
+reg [3:0] whichcon;
+connect connect(
+            .a(col),
+            .whichcon(i),
+            .b(row)
+        );
+
+initial begin
+    reset = 0;
+    whichcon = 4'b0000;
+
+
+    for (i = 0; i < 7; i = i + 1) begin
+        whichcon = i;
+        #10;
     end
 
-    integer i;
-    reg [3:0] whichcon;
-    connect connect(
-        .a(C),
-        .whichcon(i),
-        .b(col)
-    );
+    reset= 1;
+    #70;
+    reset = 0;
 
-    initial begin
-        reset = 0;
-        whichcon = 4'b0000;
-   
+    #100;
 
-        for (i = 0; i < 7; i = i + 1) begin
-            whichcon = i;
-            #10;
-        end
-
-        reset= 1;
-        #70;
-        reset = 0;
-
-        #100;
-
-        for (i = 7; i < 16; i = i + 1) begin
-            whichcon = i;
-            #10;
-        end
-
-        $finish;
+    for (i = 7; i < 16; i = i + 1) begin
+        whichcon = i;
+        #10;
     end
+
+    $finish;
+end
 endmodule
