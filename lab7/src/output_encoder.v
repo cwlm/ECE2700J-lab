d8dd9113690e1fd9c336f40e1dfd1477f7c5ac92
cwlm_ma <cwlm_ma@sjtu.edu.cn>
2024-12-09 16:12:22 +0800

    fix: update mode control signal from 'm' to 'M' for consistency

--- lab7/src/output_encoder.v
+++ lab7/src/output_encoder.v
@@ -5,7 +5,7 @@ module output_encoder(
            input wire [3:0] R0, R1, R2, R3, // roller outputs
            input wire [3:0] sum, // simple_calculator output
            input wire overflow, // simple_calculator output
-           input wire m, // mode control
+           input wire M, // mode control
            // Outputs
            output wire [6:0] C3, C2, C1, C0, // SSD outputs
            output wire overflow_led // overflow LED
@@ -24,11 +24,11 @@ ssd_driver ssd2(R2, RC2);
 ssd_driver ssd3(R3, RC3);
 ssd_driver ssd_sum(sum, sum_C);
 
-assign C0 = m ? sum_C : RC0;
-assign C1 = m ? {6'b111111, sum[3]} : RC1;
-assign C2 = m ? 7'b1111111 : RC2;
-assign C3 = m ? 7'b1111111 : RC3;
+assign C0 = M ? sum_C : RC0;
+assign C1 = M ? {6'b111111, sum[3]} : RC1;
+assign C2 = M ? 7'b1111111 : RC2;
+assign C3 = M ? 7'b1111111 : RC3;
 
-assign overflow_led = m ? overflow : 1'b0;
+assign overflow_led = M ? overflow : 1'b0;
 
 endmodule
